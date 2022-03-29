87c80a65562313489741d440f9b53c94c000acdf
JeanReyes <46287046+JeanReyes@users.noreply.github.com>
2022-03-29 10:20:37 -0300

    feat: package.json update6

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
-export { Chat } from '../src/components/components/molecules/chat/Chat';
-export { Orders } from '../src/components/components/molecules/orders/Orders'
-export { Button } from '../src/stories/components/Button'
+export { Chat } from './components/components/molecules/chat/Chat';
+export { Orders } from './components/components/molecules/orders/Orders'
+export { Button } from './stories/components/Button'
